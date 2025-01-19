0efd314b4a8e9de1cc8061c77ba0696dd4cffba1
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-01-18 22:44:55 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -309,7 +309,7 @@ app.get('/example/:name', (req, res) => {
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001; // TODO: Revert!
+const PORT = process.env.PORT || 3000; // TODO: Revert!
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
