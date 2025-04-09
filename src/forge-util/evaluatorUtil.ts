f7afb5e8f8b083d9d212d15cbb503a7a018f451f
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-04-09 12:45:17 -0400

    Closer?

--- src/forge-util/evaluatorUtil.ts
+++ src/forge-util/evaluatorUtil.ts
@@ -1,8 +1,101 @@
 import { DOMParser } from '@xmldom/xmldom';
 
 import {ForgeExprEvaluatorUtil} from 'forge-expr-evaluator';
+import { AlloyDatum, AlloyRelation, parseAlloyXML, AlloyTuple , AlloyInstance} from '../alloy-instance';
+import {DatumParsed, ParsedValue, Relation, Sig, InstanceData, ForgeTuple} from 'forge-expr-evaluator/dist/types';
 
 
+
+function toForgeTuple(tuple: AlloyTuple) : ForgeTuple {
+    return {
+        _: tuple._,
+        types: tuple.types,
+        atoms: tuple.atoms
+    };
+}
+
+function toRelation(r : AlloyRelation) : Relation {
+
+    return {
+        _: r._,
+        id: r.id,
+        name: r.name,
+        types: r.types,
+        tuples: r.tuples.map((tuple) => toForgeTuple(tuple))
+    };
+
+ }
+
+function toInstanceData(id: AlloyInstance) : InstanceData {
+
+   // TODO
+
+}
+
+
+function toParsedValue(ad : AlloyDatum) : ParsedValue {
+
+    // export interface AlloyDatum {
+    //   instances: AlloyInstance[];
+    //   bitwidth?: number;
+    //   command?: string;
+    //   loopBack?: number;
+    //   maxSeq?: number;
+    //   maxTrace?: number;
+    //   minTrace?: number;
+    //   traceLength?: number;
+    //   visualizerConfig?: VisualizerConfig;
+    // }
+
+    // export interface ParsedValue {
+    //     instances: InstanceData[];
+    //     bitwidth: number;
+    //     [key: string]: any;
+    // }
+
+    // Convert the AlloyDatum to a ParsedValue object
+    let parsedValue: ParsedValue = {
+        instances: ad.instances.map((instance) => toInstanceData(instance)),
+        bitwidth: ad.bitwidth
+        // Maybe more?
+
+    };
+
+    return parsedValue;
+
+
+}
+
+
+
+function alloyXMLToDatumParsed(datum: string): DatumParsed {
+    // Convert the AlloyDatum to a DatumParsed object
+
+    let ad: AlloyDatum = parseAlloyXML(datum);
+    // Now need to convert ad to ParsedValue
+
+    let instanceData : InstanceData[] = ad.instances.map((instance) => {
+       
+        return {
+            types : instance.types,
+            relations : instance.relations,
+            skolems : instance.skolems,
+        }
+
+
+    });
+
+
+    let parsedValue = {
+        bitwidth: ad.bitwidth,
+    };
+
+
+    return {
+        parsed: parsedValue,
+        data: datum
+    };
+}
 
 export class WrappedForgeEvaluator {
     public sourceDatum: any;
@@ -10,8 +103,6 @@ export class WrappedForgeEvaluator {
     private sourceCode: string;
 
     static getSourceCodeFromDatum(datum: any): string {
-
-
         const xmlParser = new DOMParser();
         const xmlDoc = xmlParser.parseFromString(datum, "application/xml");
       
@@ -32,16 +123,29 @@ export class WrappedForgeEvaluator {
     }
 
 
+
+
     constructor(datumAsXML: any) {
 
         // TODO: Better, more graceful failure.
+
+        // We need to create a datum here that is appropriate for the evaluator.
+        var ad: AlloyDatum = parseAlloyXML(datumAsXML);
+
+        let pv : ParsedValue = toParsedValue(ad);
+
+        let datumParsed : DatumParsed = {
+            parsed : pv,
+            data: datumAsXML
+        }
+
 
 
         // There may just *be no source code* in the datum.
 
         this.sourceDatum = datumAsXML;
         this.sourceCode = WrappedForgeEvaluator.getSourceCodeFromDatum(datumAsXML);
-        this.evaluator = new ForgeExprEvaluatorUtil(datumAsXML, this.sourceCode);
+        this.evaluator = new ForgeExprEvaluatorUtil(datumParsed, this.sourceCode);
     }
 
 
