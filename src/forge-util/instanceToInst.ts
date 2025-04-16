2d0bacb72c92bd29c8a0685fa373caec962ae3d5
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-04-16 09:01:41 -0400

    Tables broken?

--- src/forge-util/instanceToInst.ts
+++ src/forge-util/instanceToInst.ts
@@ -68,3 +68,43 @@ export function instanceToInst(instance: AlloyInstance): string {
     return `${PREFIX}\n${inst}\n${POSTFIX}`;
 }
 
+export function instanceToTables(instance: AlloyInstance) : {
+    atoms: Record<string, string[]>,
+    relations: Record<string, string[][]>
+} {
+    let instanceTypes = instance.types;
+    let instanceRelations = instance.relations;
+
+    // Create a dict where the key is the type id and the value is the atoms
+    let atomsTable : Record<string, string[]> = {};
+    for (let typeId in instanceTypes) {
+        let type = instanceTypes[typeId];
+        let atoms = type.atoms;
+        atomsTable[typeId] = atoms.map(atom => atom.id);
+    }
+
+
+    let relationTable : Record<string, string[][]> = {};
+
+    for (let relationId in instanceRelations) {
+        let relation = instanceRelations[relationId];
+        const relName = relation.name;
+        let tuples = relation.tuples;
+
+        // Now I want each tuple to be mapped to a string array.
+        let tuplesAsArrays = tuples.map(tuple => {
+            return tuple.atoms;
+        });
+
+        relationTable[relName] = tuplesAsArrays;
+    }
+
+
+    return {
+        atoms: atomsTable,
+        relations: relationTable
+    };
+}
+
+
+
