1c756927628d27e4d1aded7f2c8452afbfaa6f07
Siddhartha Prasad <siddhartha_prasad@brown.edu>
2025-05-06 09:12:11 -0400

    Package update

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(express.json({ limit: '50mb' }));
 
 // This is a hack. I'm not sure
 // how to encode the version number.
-const version = "3.1.1";
+const version = "3.1.2";
 
 const secretKey = "cope-and-drag-logging-key";
 
